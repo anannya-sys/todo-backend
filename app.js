1afcf2b4f980bda95ddf1ac5fe3c49f55c7a1cb9
anannya-sys <anannyapaliwal09@gmail.com>
2025-06-13 12:38:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./src/config/db.js";
+import studentRoutes from "./src/routes/studentRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api", studentRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello, World!" });
+});
+
+await connectDB();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+// export default app;
